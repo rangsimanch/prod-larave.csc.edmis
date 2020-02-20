6d241d38a6b21f726cbcdcab6172c78f8590172c
rangsimanch <12jniw.t@gmail.com>
2020-02-20 14:33:44 +0700

    Update show.blade.php

--- resources/views/admin/rfas/show.blade.php
+++ resources/views/admin/rfas/show.blade.php
@@ -386,7 +386,7 @@
 
 
                         @if(!empty($rfa->distribute_by->signature))
-                            <img style="display:none" id="distribute_sign" src="{{ $rfa->distribute_by->signature->getUrl() }}"/> 
+                            <img crossorigin="anonymous" style="display:none" id="distribute_sign" src="{{ $rfa->distribute_by->signature->getUrl() }}"/> 
                              <p id="distribute_sign_base64" hidden="true">
                              </p> 
                              <p hidden="true">{{ $check_distribute = 1 }}</p>
@@ -396,7 +396,7 @@
 
                         @if(!empty($rfa->comment_status->id))
                             @if(!empty($rfa->action_by->signature))
-                                <img style="display:none" id="process_sign" src="{{ $rfa->action_by->signature->getUrl() }}"/> 
+                                <img crossorigin="anonymous" style="display:none" id="process_sign" src="{{ $rfa->action_by->signature->getUrl() }}"/> 
                                  <p id="distribute_sign_base64" hidden="true">
                                  </p> 
                                  <p hidden="true">{{ $check_process = 1 }}</p>
@@ -441,7 +441,7 @@
 
 @section('scripts')
 
-<script type="text/javascript" crossorigin='anonymous'>
+<script type="text/javascript">
     
 $( document ).ready(function() {
 (function (jsPDFAPI) {
