f523575f514a7dc31a72986a748a74706aeeb160
rangsimanch <12jniw.t@gmail.com>
2020-05-13 10:49:41 +0700

    Update edit.blade.php

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -34,6 +34,8 @@
                                 <span class="help-block">{{ trans('cruds.user.fields.name_helper') }}</span>
                             </div>
 
+           
+
                             <div class="form-group {{ $errors->has('gender') ? 'has-error' : '' }}">
                                 <label class="required">{{ trans('cruds.user.fields.gender') }}</label>
                                 <select class="form-control" name="gender" id="gender" required>
@@ -55,15 +57,6 @@
                                         <span class="help-block" role="alert">{{ $errors->first('name') }}</span>
                                     @endif
                                     <span class="help-block">{{ trans('cruds.user.fields.name_helper') }}</span>
-                                </div>
-
-                                <div hidden="true" class="form-group {{ $errors->has('dob') ? 'has-error' : '' }}">
-                                    <label class="required" for="dob">{{ trans('cruds.user.fields.dob') }}</label>
-                                    <input class="form-control date" type="text" name="dob" id="dob" value="{{ old('dob', $user->dob) }}" required>
-                                    @if($errors->has('dob'))
-                                        <span class="help-block" role="alert">{{ $errors->first('dob') }}</span>
-                                    @endif
-                                    <span class="help-block">{{ trans('cruds.user.fields.dob_helper') }}</span>
                                 </div>
 
                                 <div hidden="true" class="form-group {{ $errors->has('gender') ? 'has-error' : '' }}">
