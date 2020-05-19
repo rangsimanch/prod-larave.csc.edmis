70f0bba2756822de043923b42d535b8ec37aaa03
rangsimanch <12jniw.t@gmail.com>
2020-05-19 09:32:51 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -246,7 +246,7 @@ class TaskController extends Controller
                 
                 $html .= "<div style=\"font-weight: bold; font-size: 20px; position:absolute;top:990;left:580px;\">(". $recordby  ." )</div>";
                 
-                if(is_null($task->create_by_user->signature->getPath())){
+                if(!is_null($task->create_by_user->signature->getPath())){
                     $html .= "<div style=\"font-weight: bold; font-size: 20px; position:absolute;top:960;left:630px;\">
                             <img width=\"40%\" height=\"auto\" src=\"" . $task->create_by_user->signature->getPath()
                             . "\"></div>";
