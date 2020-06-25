c7b105ca8e708901c9c7989ace53034f0b3a76db
rangsimanch <12jniw.t@gmail.com>
2020-06-25 08:57:05 +0700

    Update RfaController.php

--- app/Http/Controllers/Admin/RfaController.php
+++ app/Http/Controllers/Admin/RfaController.php
@@ -1266,10 +1266,9 @@ class RfaController extends Controller
          //Submittals Page
         if(count($rfa->submittals_file) > 0 ){
             foreach($rfa->submittals_file as $submittal){
-                $path = urlencode($submittal->getUrl());
+                $path = $submittal->getUrl();
                 if(in_array(pathinfo($path,PATHINFO_EXTENSION),$allowed)){
-                    $fileContent = file_get_contents($submittal->getUrl(),'rb');
-                    $pagecount = $mpdf->SetSourceFile(StreamReader::createByString($fileContent));
+                    $pagecount = $mpdf->SetSourceFile($path);
                     for ($i=1; $i<=($pagecount); $i++) {
                         $mpdf->AddPage();
                         $import_page = $mpdf->ImportPage($i);
