d55dd1610d1e285be1602e8bb448833995b45198
rangsimanch <12jniw.t@gmail.com>
2020-07-06 22:29:44 +0700

    BoQs

---
+++ resources/views/admin/boQs/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.admin')
+@section('content')
+<div class="content">
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    {{ trans('global.create') }} {{ trans('cruds.boQ.title_singular') }}
+                </div>
+                <div class="panel-body">
+                    <form method="POST" action="{{ route("admin.bo-qs.store") }}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+                            <label for="name">{{ trans('cruds.boQ.fields.name') }}</label>
+                            <input class="form-control" type="text" name="name" id="name" value="{{ old('name', '') }}">
+                            @if($errors->has('name'))
+                                <span class="help-block" role="alert">{{ $errors->first('name') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.boQ.fields.name_helper') }}</span>
+                        </div>
+                        <div class="form-group {{ $errors->has('code') ? 'has-error' : '' }}">
+                            <label for="code">{{ trans('cruds.boQ.fields.code') }}</label>
+                            <input class="form-control" type="text" name="code" id="code" value="{{ old('code', '') }}">
+                            @if($errors->has('code'))
+                                <span class="help-block" role="alert">{{ $errors->first('code') }}</span>
+                            @endif
+                            <span class="help-block">{{ trans('cruds.boQ.fields.code_helper') }}</span>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-danger" type="submit">
+                                {{ trans('global.save') }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+@endsection
