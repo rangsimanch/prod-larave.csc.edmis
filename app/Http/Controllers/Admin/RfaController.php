15ae28f31d4fce85e404d901156bec492a125242
rangsimanch <12jniw.t@gmail.com>
2020-08-21 13:16:31 +0700

    Submittal

--- app/Http/Controllers/Admin/RfaController.php
+++ app/Http/Controllers/Admin/RfaController.php
@@ -1171,7 +1171,7 @@ class RfaController extends Controller
               print "Creating an mPDF object failed with" . $e->getMessage();
           }
         //RFA Page
-        $pagecount = $mpdf->SetSourceFile(public_path('pdf-asset/RFA-Form_new.pdf'));
+        $pagecount = $mpdf->SetSourceFile(public_path('pdf-asset/RFA-Form copy.pdf'));
         $tplId = $mpdf->ImportPage($pagecount);
         $mpdf->UseTemplate($tplId);        
           //Title
