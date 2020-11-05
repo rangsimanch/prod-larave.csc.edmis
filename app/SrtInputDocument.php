71c796dc83e2849d9701014f408b06ae9646287d
rangsimanch <12jniw.t@gmail.com>
2020-11-05 11:58:35 +0700

    Update SrtInputDocument.php

--- app/SrtInputDocument.php
+++ app/SrtInputDocument.php
@@ -175,9 +175,9 @@ class SrtInputDocument extends Model implements HasMedia
     {
         return $this->belongsTo(Team::class, 'team_id');
     }
-
+    
     public function create_by_construction_contract_id()
     {
-        return $this->belongsTo(ConstructionContract::class, 'constuction_contract_id');
+        return $this->belongsTo(ConstructionContract::class, 'construction_contract_id');
     }
 }
