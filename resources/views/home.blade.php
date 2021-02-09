eaf88e125eac47b2fb8d83f9815718fe42b26cc5
rangsimanch <12jniw.t@gmail.com>
2021-02-09 09:11:12 +0700

    config home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+          
+                             
                     
                 </div>
             </div>
