e46a628060e9db96e9ab749b2f0b7017c046ea53
rangsimanch <12jniw.t@gmail.com>
2021-03-08 22:51:19 +0700

    Update RfaController.php

--- app/Http/Controllers/Admin/RfaController.php
+++ app/Http/Controllers/Admin/RfaController.php
@@ -1091,8 +1091,8 @@ class RfaController extends Controller
             $logo_path =  public_path('png-asset/NWR_logo.png');
             $stamp_path =  public_path('png-asset/NWR_stamp.png');
             $signature_path =  public_path('png-asset/NWR_signature.png');
-            $signature_size_h = 60;
-            $signature_size_w = 60;
+            $signature_size_h = 80;
+            $signature_size_w = 80;
             $contract_name = 'Contract ' . $rfa->construction_contract->code . ' : ' . $rfa->construction_contract->name;
 
         }
