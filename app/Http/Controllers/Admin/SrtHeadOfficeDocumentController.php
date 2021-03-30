8dcfbf2acc79ecd2c678d55c1142ca0a1d17f3bf
rangsimanch <12jniw.t@gmail.com>
2021-03-31 00:23:24 +0700

    Update SrtHeadOfficeDocumentController.php

--- app/Http/Controllers/Admin/SrtHeadOfficeDocumentController.php
+++ app/Http/Controllers/Admin/SrtHeadOfficeDocumentController.php
@@ -225,7 +225,7 @@ class SrtHeadOfficeDocumentController extends Controller
         if($data['save_for'] == "Closed"){
             $CompleteMerger = PDFMerger::init();
             $fileUpload_1 = $srtInputDocument->getMedia('file_upload')->first();
-            $CompleteMerger->addPDF(public_path($fileUpload_1)->getMedia());
+            $CompleteMerger->addPDF($fileUpload_1->getUrl());
             foreach ($request->input('file_upload', []) as $file) {
                 if (count($media) === 0 || !in_array($file, $media)) {
                     $CompleteMerger->addPDF(storage_path('tmp/uploads/' . $file), 'all');  
