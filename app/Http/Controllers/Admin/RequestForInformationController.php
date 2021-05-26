c47181c49b571cb6509ad258f398bbc0089924f2
rangsimanch <12jniw.t@gmail.com>
2021-05-27 01:59:21 +0700

    Update RequestForInformationController.php

--- app/Http/Controllers/Admin/RequestForInformationController.php
+++ app/Http/Controllers/Admin/RequestForInformationController.php
@@ -519,7 +519,7 @@ class RequestForInformationController extends Controller
         $html .= "<div style=\"font-size: 13px; position:absolute;top:373px;left:520px;\">" . $incoming_no . "</div>";
         $html .= "<div style=\"font-size: 13px; position:absolute;top:390px;left:520px;\">" . $submit_date . "</div>";
 
-        $html .= "<div style=\"font-size: 13px; position:absolute;top:430px;left:90px;text-align: center;\">" . $description . '.' . "</div>";
+        $html .= "<div style=\"font-size: 13px; position:absolute;top:430px;left:30px;text-align: center;\">" . $description . '.' . "</div>";
 
 
 
