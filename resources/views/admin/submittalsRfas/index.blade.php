8fb92a7c5bc48a6e35f8aa58596c1e26bd869c14
rangsimanch <12jniw.t@gmail.com>
2021-07-27 14:34:50 +0700

    Update index.blade.php

--- resources/views/admin/submittalsRfas/index.blade.php
+++ resources/views/admin/submittalsRfas/index.blade.php
@@ -7,6 +7,10 @@
                 <a class="btn btn-success" href="{{ route("admin.submittals-rfas.create") }}">
                     {{ trans('global.add') }} {{ trans('cruds.submittalsRfa.title_singular') }}
                 </a>
+                <button class="btn btn-warning" data-toggle="modal" data-target="#csvImportModal">
+                    {{ trans('global.app_csvImport') }}
+                </button>
+                @include('csvImport.modal', ['model' => 'SubmittalsRfa', 'route' => 'admin.submittals-rfas.parseCsvImport'])
             </div>
         </div>
     @endcan
