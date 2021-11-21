758bb2d0643733653cfbd28827dbf131716ec2d2
rangsimanch <12jniw.t@gmail.com>
2021-11-21 23:54:49 +0700

    Update RfaController.php

--- app/Http/Controllers/Admin/RfaController.php
+++ app/Http/Controllers/Admin/RfaController.php
@@ -1329,7 +1329,7 @@ class RfaController extends Controller
         $submittalsRfa = $submittalsRfa ?? '';
         
         $document_name = wordwrap($rfa->attach_file_name ?? '',300,"<br>\n");
-        $note_1 = wordwrap($rfa->note_1 ?? '',450,"<br>\n");
+        $note_1 = wordwrap($rfa->note_1 ?? '',600,"<br>\n");
         $note_2 = wordwrap($rfa->note_2 ?? '',400,"<br>\n");
         $note_3 = wordwrap($rfa->note_3 ?? '',400,"<br>\n");
         $note_4 = wordwrap($rfa->note_4 ?? '',400,"<br>\n");
