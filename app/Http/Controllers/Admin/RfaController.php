0be67692e9790d93f73ea7941f9e858896ef604f
rangsimanch <12jniw.t@gmail.com>
2021-12-01 16:05:39 +0700

    Update RfaController.php

--- app/Http/Controllers/Admin/RfaController.php
+++ app/Http/Controllers/Admin/RfaController.php
@@ -1181,7 +1181,7 @@ class RfaController extends Controller
             $signature_path =  public_path('png-asset/SPTK_signature.png');
             $signature_size_h = 60;
             $signature_size_w = 60;
-            $signature_position_top = 425;
+            $signature_position_top = 430;
             $signature_position_left = 275;
             $stamp_size_h = 120;
             $stamp_size_w = 120;
