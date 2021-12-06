74647ab640c05b5915b7aaa0410530c49cdd3cce
rangsimanch <12jniw.t@gmail.com>
2021-12-06 16:21:19 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -272,8 +272,8 @@ class TaskController extends Controller
                 $description = $task->description ?? '';
                 $description_len = strlen($description);
 
-                if($description_len > 1000){
-                    $description_set = str_split($description, 1000);
+                if($description_len > 500){
+                    $description_set = str_split($description, 500);
                 }
                 else{
                     $description_set = $description;
@@ -308,7 +308,7 @@ class TaskController extends Controller
                 
 
                     $html .= "<div style=\" padding-left: 80px; padding-right:80px; \">
-                                    <div style=\"vertical-align: top; max-width: 50%; display: inline-block; font-size: 20px;\">".  nl2br(str_replace(';','\n',$description)) ."</div>
+                                    <div style=\"vertical-align: top; max-width: 50%; display: inline-block; font-size: 18px;\">".  nl2br(str_replace(';','\n',$description)) ."</div>
                                     </div>";   
                     $html .= "<div style=\"font-weight: bold; font-size: 20px; position:absolute;top:990;left:580px;\">(". $recordby  .")</div>";
                     if(!is_null($task->create_by_user->signature)){
