51ccc142a17d8e2270d210721f2c315f870e345b
rangsimanch <12jniw.t@gmail.com>
2021-12-06 18:38:43 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -271,7 +271,6 @@ class TaskController extends Controller
                 $pagecount = $mpdf->SetSourceFile(public_path('pdf-asset/activity.pdf'));
                 $description = $task->description ?? '';
                 $description_set = str_split($description, 1500);
-                foreach($description_set as $dc) {
                 
                     $mpdf->AddPage();
                     // Import the last page of the source PDF file
@@ -301,7 +300,7 @@ class TaskController extends Controller
                 
 
                     $html .= "<div style=\" padding-left: 80px; padding-right:80px; \">
-                                    <div style=\"vertical-align: top; max-width: 50%; display: inline-block; font-size: 18px;\">".  nl2br(str_replace(';','\n',$dc)) ."</div>
+                                    <div style=\"vertical-align: top; max-width: 50%; display: inline-block; font-size: 18px;\">".  nl2br(str_replace(';','\n',$description)) ."</div>
                                     </div>";   
                     $html .= "<div style=\"font-weight: bold; font-size: 20px; position:absolute;top:990;left:580px;\">(". $recordby  .")</div>";
                     if(!is_null($task->create_by_user->signature)){
@@ -309,9 +308,11 @@ class TaskController extends Controller
                                 <img width=\"60%\" height=\"60%\" src=\"" . $task->create_by_user->signature->getPath()
                                 . "\"></div>";
                     }
-                }
-                    
-                    // $html .= "<div style=\"font-weight: bold; font-size: 14px; position:absolute;top:0;left:0;\">". $index_dc  ."</div>";
+                    $index_dc = 0;
+                    foreach($description_set as $dc) {
+                        $html .= "<div style=\"font-weight: bold; font-size: 8px; position:absolute;top:0;left:0;\">". $dc  ."</div>";
+                        
+                    }
 
                                 
                 try{
