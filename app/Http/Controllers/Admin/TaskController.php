26487a0c1fff30a24a94cd7fad57576afd0d87bd
rangsimanch <12jniw.t@gmail.com>
2021-12-11 17:50:19 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -293,7 +293,7 @@ class TaskController extends Controller
                 $html .= "<div style=\"font-weight: bold; font-size: 20px; position:absolute;top:155px;left:500px;\">Temperature : ". $temperature  ." °C</div>";
                     
                 $html .= "<br><br><br><br><br><br><br><br>
-                                <div style=\" padding-left: 80px; padding-right:80px;  position:absolute;top:200px;\">
+                                <div style=\" padding-left: 80px; padding-right:80px;  position:top:200px;\">
                                 <div style=\"text-align: center;font-weight: bold; font-size: 22px;\">". nl2br(str_replace(';',"\r\n",$activity_name))  ."</div>
                                 </div>";
                 
