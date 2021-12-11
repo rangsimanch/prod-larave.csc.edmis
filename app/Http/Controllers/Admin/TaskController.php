5e75984452978fb1c9d165b415c96e5f87c6930e
rangsimanch <12jniw.t@gmail.com>
2021-12-11 18:22:04 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -335,7 +335,7 @@ class TaskController extends Controller
 
                             if(in_array(pathinfo(public_path($task->attachment[0]->getUrl()),PATHINFO_EXTENSION),$allowed)){
                                 $img = (string) Image::make($task->attachment[0]->getPath())->resize(null, 180, function ($constraint) {
-                                    $constraint->aspectRatio();
+                                    $constraint;
                                 })
                                 ->encode('data-url');
 
@@ -345,7 +345,7 @@ class TaskController extends Controller
                             }
                             if(in_array(pathinfo(public_path($task->attachment[1]->getUrl()),PATHINFO_EXTENSION),$allowed)){
                                 $img = (string) Image::make($task->attachment[1]->getPath())->resize(null, 180, function ($constraint) {
-                                    $constraint->aspectRatio();
+                                    $constraint;
                                 })->encode('data-url');
                                 
                                 $html .= " <img width=\"25%\" height=\"25%\" src=\"" 
