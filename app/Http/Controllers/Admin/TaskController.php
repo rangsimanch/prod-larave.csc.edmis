cebf1002842b93eddfab24390292d2afcf969e45
rangsimanch <12jniw.t@gmail.com>
2022-01-05 01:25:01 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -249,12 +249,12 @@ class TaskController extends Controller
                     $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 40px;\">" .  $dateType . "</div>";        
                     $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 40px;\"><img src=\"". public_path('png-asset/train_cover.png') ."\"\></div>";        
                     $html .= "<br></br><div style=\"text-align: center; font-weight: bold; font-size: 40px;\"> Supervision Diary </div>";        
-                    $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 28px;\">Contract Section No. : " . "All work of the whole line"  . "</div>";   
-                    $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 28px;\">Supervision mileage chainage : DK 000 + 300 to DK 253 + 000 Section </div>";        
-                    $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 28px;\">BANGKOK-NAKHON RATCHASIMA HIGH SPEED RAILWAY</div>";        
+                    $html .= "<div style=\"text-align: left; font-weight: bold; font-size: 28px;\">Contract Section No. : " . "All work of the whole line"  . "</div>";   
+                    $html .= "<div style=\"text-align: left; font-weight: bold; font-size: 28px;\">Supervision mileage chainage : DK 000 + 300 to DK 253 + 000 Section </div>";        
+                    $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 30px;\">BANGKOK-NAKHON RATCHASIMA HIGH SPEED RAILWAY</div>";        
                     $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 28px;\">Supevision Unit : Chaina Railway International Corporation and Chaina Railway Design Group</div>";   
-                    $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 28px;\">Thailand Railway ProjecT Depart of the Consortium</div>";        
-                    $html .= "<br></br><div style=\"text-align: center; font-weight: bold; font-size: 28px;\">By ". $recordby ."</div>";        
+                    $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 28px;\">Thailand Railway Project Depart of the Consortium</div>";        
+                    $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 28px;\">By ". $recordby ."</div>";        
                     $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 25px;\">". $jobtitle ."</div>";        
                     $html .= "<div style=\"text-align: center; font-weight: bold; font-size: 25px;\">". $team ."</div>"; 
                            
