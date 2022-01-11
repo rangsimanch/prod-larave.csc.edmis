f3d7ab03781214161f05c0d83eba1a81eb02a243
rangsimanch <12jniw.t@gmail.com>
2022-01-11 23:18:55 +0700

    Update index.blade.php

--- resources/views/admin/complaints/index.blade.php
+++ resources/views/admin/complaints/index.blade.php
@@ -24,13 +24,14 @@
                 <table border="0" cellspacing="5" cellpadding="5">
                     <tbody><tr>
                         <td>Minimum date:</td>
-                        <td><input class="form-control date" type="text" id="min" name="min"></td>
+                        <td><input type="text" id="min" name="min"></td>
                     </tr>
                     <tr>
                         <td>Maximum date:</td>
-                        <td><input class="form-control date" type="text" id="max" name="max"></td>
+                        <td><input type="text" id="max" name="max"></td>
                     </tr>
-                </tbody></table>
+                    </tbody>
+                </table>
                     <table id="complaintTable" class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-Complaint">
                         <thead>
                             <tr>
