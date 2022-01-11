a32ab06250e414243b5682ddfcf90a1238699ce0
rangsimanch <12jniw.t@gmail.com>
2022-01-12 00:25:13 +0700

    Update index.blade.php

--- resources/views/admin/complaints/index.blade.php
+++ resources/views/admin/complaints/index.blade.php
@@ -307,5 +307,8 @@ $(function() {
   $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
       $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
   });
+});
+
+</script>
 
 @endsection
