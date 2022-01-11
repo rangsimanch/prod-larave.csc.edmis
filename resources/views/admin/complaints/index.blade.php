bf546ccb8b7c06e6d7bf5145ff58a14a2100ec87
rangsimanch <12jniw.t@gmail.com>
2022-01-12 00:31:02 +0700

    Update index.blade.php

--- resources/views/admin/complaints/index.blade.php
+++ resources/views/admin/complaints/index.blade.php
@@ -305,7 +305,8 @@ $(function() {
   });
 
   $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
-      $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+    //   $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+      $(this).val(picker.startDate.format('MM/DD/YYYY'));
   });
 });
 
