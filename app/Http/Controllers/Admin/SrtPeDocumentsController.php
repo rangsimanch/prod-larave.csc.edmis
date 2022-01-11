8fa4822bdf5cf360bbb845b129780c09e1d9bee1
rangsimanch <12jniw.t@gmail.com>
2022-01-12 01:08:30 +0700

    Update SrtPeDocumentsController.php

--- app/Http/Controllers/Admin/SrtPeDocumentsController.php
+++ app/Http/Controllers/Admin/SrtPeDocumentsController.php
@@ -104,21 +104,13 @@ class SrtPeDocumentsController extends Controller
                 return $row->note ? $row->note : "";
             });
             $table->editColumn('file_upload', function ($row) {
-                // if (!$row->file_upload) {
-                //     return '';
-                // }
-
-                if (!$row->refer_documents->file_upload_4) {
+                if (!$row->file_upload) {
                     return '';
                 }
 
                 $links = [];
 
-                // foreach ($row->file_upload as $media) {
-                //     $links[] = '<a href="' . $media->getUrl() . '" target="_blank">' . trans('global.downloadFile') . '</a>';
-                // }
-
-                foreach ($row->refer_documents->file_upload_4 as $media) {
+                foreach ($row->file_upload as $media) {
                     $links[] = '<a href="' . $media->getUrl() . '" target="_blank">' . trans('global.downloadFile') . '</a>';
                 }
 
