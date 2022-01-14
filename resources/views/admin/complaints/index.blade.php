8544a5b276cefaf12ec4cd2d985fdc57624a1d00
rangsimanch <12jniw.t@gmail.com>
2022-01-14 21:06:11 +0700

    Update index.blade.php

--- resources/views/admin/complaints/index.blade.php
+++ resources/views/admin/complaints/index.blade.php
@@ -302,7 +302,7 @@ $(document).ready(function() {
        opens: "left",
        locale: {
          cancelLabel: 'Clear',
-         format: 'DD-MM-YYYY'
+         format: 'DD/MM/YYYY'
        }
      });
 
@@ -312,7 +312,7 @@ $(document).ready(function() {
 
      // Function for converting a dd/mmm/yyyy date value into a numeric string for comparison (example 01-Dec-2010 becomes 20101201
      function parseDateValue(rawDate) {
-       var d = moment(rawDate, "DD-MM-YYYY").format('DD/MM/YYYY');
+       var d = moment(rawDate, "DD/MM/YYYY").format('DD/MM/YYYY');
        var dateArray = d.split("/");
        var parsedDate = dateArray[2] + dateArray[1] + dateArray[0];
        return parsedDate;
@@ -322,7 +322,7 @@ $(document).ready(function() {
      $(".daterange").on('apply.daterangepicker', function(ev, picker) {
        ev.preventDefault();
        //if blank date option was selected
-       if ((picker.startDate.format('DD-MM-YYYY') == "01-01-0001") && (picker.endDate.format('DD-MM-YYYY')) == "01-01-0001") {
+       if ((picker.startDate.format('DD/MM/YYYY') == "01/01/0001") && (picker.endDate.format('DD/MM/YYYY')) == "01/01/0001") {
          $(this).val('Blank');
          val = "^$";
          table.column(dataIdx)
@@ -334,8 +334,8 @@ $(document).ready(function() {
          $(this).val(picker.startDate.format('DD/MM/YYYY') + ' to ' + picker.endDate.format('DD/MM/YYYY'));
 
          //run date filter
-         startDate = picker.startDate.format('DD-MM-YYYY');
-         endDate = picker.endDate.format('DD-MM-YYYY');
+         startDate = picker.startDate.format('DD/MM/YYYY');
+         endDate = picker.endDate.format('DD/MM/YYYY');
 
          var dateStart = parseDateValue(startDate);
          var dateEnd = parseDateValue(endDate);
@@ -361,7 +361,7 @@ $(document).ready(function() {
         //  console.log(val);
         //  console.log(table.column(dataIdx).data());
          table.column(dataIdx)
-           .search(val ? "^" + val + "$" : "^" + "-" + "$", true, false, true)
+           .search(val ? "^" + val + "$" : "^" + "/" + "$", true, false, true)
            .draw();
        }
      });
