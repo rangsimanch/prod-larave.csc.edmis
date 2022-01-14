f7e854d7782f6ad44c2f2bcc998477746c8e6bb2
rangsimanch <12jniw.t@gmail.com>
2022-01-15 02:46:42 +0700

    Update index.blade.php

--- resources/views/admin/complaints/index.blade.php
+++ resources/views/admin/complaints/index.blade.php
@@ -280,146 +280,103 @@
             });
         }) 
 
-        var minDate, maxDate;
-        // Custom filtering function which will search data in column four between two values
-        
-        $('.daterange').daterangepicker({
-            ranges: {
-                "Today": [moment(), moment()],
-                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                '7 last days': [moment().subtract(6, 'days'), moment()],
-                '30 last days': [moment().subtract(29, 'days'), moment()],
-                'This month': [moment().startOf('month'), moment().endOf('month')],
-                'Last month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-            }
-            ,
-            autoUpdateInput: false,
-            opens: "left",
-            locale: {
-                cancelLabel: 'Clear',
-                format: 'DD/MM/YYYY'
-            }
-        });
-
-        $(".daterange").on('apply.daterangepicker', function (ev, picker) {
-            $(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
-            mindate = picker.startDate.format('YYYY-MM-DD');
-            maxdate = picker.endDate.format('YYYY-MM-DD');
-            $.fn.dataTable.ext.search.push(
-            function( settings, data, dataIndex ) {
-                    var min = minDate.val();
-                    var max = maxDate.val();
-                    console.log("Working")
-                    var date = new Date( data[5] );
-            
-                    if (
-                        ( min === null && max === null ) ||
-                        ( min === null && date <= max ) ||
-                        ( min <= date   && max === null ) ||
-                        ( min <= date   && date <= max )
-                    ) {
-                        return true;
-                    }
-                    return false;
-                }
-            );
-        });
-
-
         // date range
-    //     $('.daterange').daterangepicker({
-    //     ranges: {
-    //         "Today": [moment(), moment()],
-    //         'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-    //         '7 last days': [moment().subtract(6, 'days'), moment()],
-    //         '30 last days': [moment().subtract(29, 'days'), moment()],
-    //         'This month': [moment().startOf('month'), moment().endOf('month')],
-    //         'Last month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-    //     }
-    //     ,
-    //     autoUpdateInput: false,
-    //     opens: "left",
-    //     locale: {
-    //         cancelLabel: 'Clear',
-    //         format: 'DD/MM/YYYY'
-    //     }
-    // });
+        $('.daterange').daterangepicker({
+        ranges: {
+            "Today": [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            '7 last days': [moment().subtract(6, 'days'), moment()],
+            '30 last days': [moment().subtract(29, 'days'), moment()],
+            'This month': [moment().startOf('month'), moment().endOf('month')],
+            'Last month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+        ,
+        autoUpdateInput: false,
+        opens: "left",
+        locale: {
+            cancelLabel: 'Clear',
+            format: 'DD/MM/YYYY'
+        }
+    });
 
-    // let startDate;
-    // let endDate;
-    // let dataIdx = 5;  //current data column to work with
+    let startDate;
+    let endDate;
+    let dataIdx = 5;  //current data column to work with
 
-    // // Function for converting a dd/mmm/yyyy date value into a numeric string for comparison (example 01-Dec-2010 becomes 20101201
-    // function parseDateValue(rawDate) {
-    //     var d = moment(rawDate, "DD/MM/YYYY").format("DD/MM/YYYY");
-    //     var dateArray = d.split("/");
-    //     var parsedDate = dateArray[2] + dateArray[1] + dateArray[0];
-    //     return parsedDate;
-    // }
+    // Function for converting a dd/mmm/yyyy date value into a numeric string for comparison (example 01-Dec-2010 becomes 20101201
+    function parseDateValue(rawDate) {
+        var d = moment(rawDate, "DD/MM/YYYY").format("DD/MM/YYYY");
+        var dateArray = d.split("/");
+        var parsedDate = dateArray[2] + dateArray[1] + dateArray[0];
+        return parsedDate;
+    }
 
-    // function covertDateValue(text) {
-    //     let year = text.slice(0, 4);
-    //     let month = text.slice(4, 6);
-    //     let day = text.slice(6, 8);
-    //     var parsedDate = year + "-" + month + "-" + day;
-    //     return parsedDate;
-    // }
+    function covertDateValue(text) {
+        let year = text.slice(0, 4);
+        let month = text.slice(4, 6);
+        let day = text.slice(6, 8);
+        var parsedDate = year + "-" + month + "-" + day;
+        return parsedDate;
+    }
 
-    // //filter on daterange
-    // $(".daterange").on('apply.daterangepicker', function (ev, picker) {
-    //     // ev.preventDefault();
-    //     //if blank date option was selected
-    //     if ((picker.startDate.format('DD/MM/YYYY') == "01/01/0001") && (picker.endDate.format('DD/MM/YYYY')) == "01/01/0001") {
-    //         $(this).val('');
-    //         val = "^$";
-    //         table.column(dataIdx)
-    //            .search(val, true, false)
-    //            .draw();
-    //     }
-    //     else {
-    //         //set field value
-    //         $(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
-    //         //run date filter
-    //         startDate = picker.startDate.format('DD/MM/YYYY');
-    //         endDate = picker.endDate.format('DD/MM/YYYY');
+    //filter on daterange
+    $(".daterange").on('apply.daterangepicker', function (ev, picker) {
+        ev.preventDefault();
+        table.column(dataIdx)
+                .search('')
+                .draw();
+        //if blank date option was selected
+        if ((picker.startDate.format('DD/MM/YYYY') == "01/01/0001") && (picker.endDate.format('DD/MM/YYYY')) == "01/01/0001") {
+            $(this).val('');
+            val = "^$";
+            table.column(dataIdx)
+               .search(val, true, false)
+               .draw();
+        }
+        else {
+            //set field value
+            $(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
+            //run date filter
+            startDate = picker.startDate.format('DD/MM/YYYY');
+            endDate = picker.endDate.format('DD/MM/YYYY');
 
-    //         var dateStart = parseDateValue(startDate);
-    //         var dateEnd = parseDateValue(endDate);
+            var dateStart = parseDateValue(startDate);
+            var dateEnd = parseDateValue(endDate);
             
-    //         var filteredData = table
-    //                 .column(dataIdx)
-    //                 .data()
-    //                 .filter(function (value, index) {
-    //                     console.log("Workling")
-    //                     var evalDate = value === "" ? 0 : parseDateValue(value);
-    //                     if ((isNaN(dateStart) && isNaN(dateEnd)) || (evalDate >= dateStart && evalDate <= dateEnd)) {
-    //                         return true;
-    //                     }
-    //                     return false;
-    //                 });
-    //         var val = "";
-    //         for (var count = 0; count < filteredData.length; count++) {
-    //             var filterDate = new Date(covertDateValue(parseDateValue(filteredData[count])));
-    //             let searchData = filterDate.getFullYear() + "-" + ("0" + (filterDate.getMonth() + 1)).slice(-2) + "-" + ("0" + filterDate.getDate()).slice(-2);
-    //             val += searchData + "|";
-    //         }
-    //         val = val.slice(0, -1);
-    //         table.column(dataIdx)
-    //             .search(val ? "^" + val + "$" : "^" + "-" + "$", true, false)
-    //             .draw();
+            var filteredData = table
+                    .column(dataIdx)
+                    .data()
+                    .filter(function (value, index) {
+                        console.log("Workling")
+                        var evalDate = value === "" ? 0 : parseDateValue(value);
+                        if ((isNaN(dateStart) && isNaN(dateEnd)) || (evalDate >= dateStart && evalDate <= dateEnd)) {
+                            return true;
+                        }
+                        return false;
+                    });
+            var val = "";
+            for (var count = 0; count < filteredData.length; count++) {
+                var filterDate = new Date(covertDateValue(parseDateValue(filteredData[count])));
+                let searchData = filterDate.getFullYear() + "-" + ("0" + (filterDate.getMonth() + 1)).slice(-2) + "-" + ("0" + filterDate.getDate()).slice(-2);
+                val += searchData + "|";
+            }
+            val = val.slice(0, -1);
+            table.column(dataIdx)
+                .search(val ? "^" + val + "$" : "^" + "-" + "$", true, false)
+                .draw();
             
-    //         console.log(filteredData.length);
-    //         console.log(val ? "^" + val + "$" : "^" + "-" + "$");
-    //         }
-    //     });
+            console.log(filteredData.length);
+            console.log(val ? "^" + val + "$" : "^" + "-" + "$");
+            }
+        });
 
-    //     $(".daterange").on('cancel.daterangepicker', function (ev, picker) {
-    //         ev.preventDefault();
-    //         $(this).val('');
-    //         table.column(dataIdx)
-    //             .search('')
-    //             .draw();
-    //     });
+        $(".daterange").on('cancel.daterangepicker', function (ev, picker) {
+            ev.preventDefault();
+            $(this).val('');
+            table.column(dataIdx)
+                .search('')
+                .draw();
+        });
     }); 
 
 </script>
