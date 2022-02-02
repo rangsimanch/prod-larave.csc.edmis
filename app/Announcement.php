6f32c49766eea19d94a31b65363aaf9ef2497e6e
rangsimanch <12jniw.t@gmail.com>
2022-02-03 02:48:02 +0700

    Update Announcement.php

--- app/Announcement.php
+++ app/Announcement.php
@@ -83,4 +83,9 @@ class Announcement extends Model implements HasMedia
     {
         return $this->belongsTo(Team::class, 'team_id');
     }
+
+    public function create_by_construction_contract_id()
+    {
+        return $this->belongsTo(ConstructionContract::class, 'construction_contract_id');
+    }
 }
