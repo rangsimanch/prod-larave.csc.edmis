4524fc7d880a0dc794acddc9791144e3b9b61ccf
rangsimanch <12jniw.t@gmail.com>
2022-02-10 14:38:27 +0700

    Update Controller.php

--- app/Http/Controllers/Admin/Controller.php
+++ app/Http/Controllers/Admin/Controller.php
@@ -16,24 +16,16 @@ class HomeController
         $date = new DateTime();
         $today = $date->format('Y-m-d');
         if(Auth::id() != 1){
-        $chart = (new LarapexChart)->barhart()
+        $chart = (new LarapexChart)->pieChart()
             ->setTitle('RFA')
             ->addData([
                 \App\Rfa::where('construction_contract_id', '=', session('construction_contract_id'))
                     ->whereDate('submit_date', '=', $today)->count(),
-                    \App\Rfa::where('document_status_id', '=', '1')
-                    ->whereDate('submit_date', '=', $today)->count(),
-                    \App\Rfa::where('document_status_id', '=', '2')
-                    ->whereDate('submit_date', '=', $today)->count(),
-                    \App\Rfa::where('document_status_id', '=', '3')
-                    ->whereDate('submit_date', '=', $today)->count(),
-                    \App\Rfa::where('document_status_id', '=', '4')
-                    ->whereDate('submit_date', '=', $today)->count(),
                 
                 ])
-            ->setColors(['#008FFB','#00E396','#feb019','#ff455f','#775dd0'])
-            ->setXAxis(['Summary', 'New', 'Distribute', 'Reviwed', 'Done'])
-            ->setGrid();
+            ->setColors(['#ffc63b'])
+            ->setLabels(['RFA Count']
+            );
         }
         else{
             $chart = (new LarapexChart)->barChart()
