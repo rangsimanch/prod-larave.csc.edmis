597dbc543ac55b26e9f14ebf879ef6457947d64b
rangsimanch <12jniw.t@gmail.com>
2022-02-12 03:38:58 +0700

    Update index.blade.php

--- resources/views/admin/rfas/index.blade.php
+++ resources/views/admin/rfas/index.blade.php
@@ -153,7 +153,7 @@
                                 <td>
                                 </td>
                                 <td>
-                                    <input type="text" name="daterange" id="daterange" class="form-control daterange" value="" autocomplete="off" placeholder="Select Period..">
+                                    {{-- <input type="text" name="daterange" id="daterange" class="form-control daterange" value="" autocomplete="off" placeholder="Select Period.."> --}}
                                 </td>
                                 <td>
                                 </td>
@@ -484,7 +484,7 @@ table.on('column-visibility.dt', function(e, settings, column, state) {
 
     let startDate;
     let endDate;
-    let dataIdx = 39;  //current data column to work with
+    let dataIdx = 2;  //current data column to work with
 
     // Function for converting a dd/mmm/yyyy date value into a numeric string for comparison (example 01-Dec-2010 becomes 20101201
     function parseDateValue(rawDate) {
