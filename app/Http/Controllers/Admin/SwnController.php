48fad948b030687e18bcf09a306d8b48e4500a12
rangsimanch <12jniw.t@gmail.com>
2022-02-11 09:50:26 +0700

    Update SwnController.php

--- app/Http/Controllers/Admin/SwnController.php
+++ app/Http/Controllers/Admin/SwnController.php
@@ -424,7 +424,7 @@ class SwnController extends Controller
             $html .= "<div style=\"font-size: 18px;font-weight: bold; position:absolute;top:260px;left:632px;\">" . "X"  . "</div>";
         }
         $html .= "<div style=\"font-size: 18px; font-weight: bold; position:absolute;top:287px;left:230px;\"> : </div>";
-        $html .= "<div style=\"font-size: 16px; padding-right:80px; position:absolute;top:270px;left:235px;\">" . $ref_doc  . "</div>";
+        $html .= "<div style=\"font-size: 14px; padding-right:83px; position:absolute;top:270px;left:235px;\">" . $ref_doc  . "</div>";
         $html .= "<div style=\"font-size: 18px; font-weight: bold; padding-right:80px; position:absolute;top:335px;left:100px;\">" . "Description(Content details):"  . "</div>";
         $html .= "<div style=\"font-size: 16px; padding-right:80px; position:absolute;top:338px;left:120px;\">" . $description  . "</div>";
         if($issuer_name != '')
