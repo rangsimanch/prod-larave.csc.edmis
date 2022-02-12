ddd924b7b89d21569ecb333bc2a36c9e2b74ecfb
rangsimanch <12jniw.t@gmail.com>
2022-02-12 11:50:23 +0700

    Update RfaController.php

--- app/Http/Controllers/Admin/RfaController.php
+++ app/Http/Controllers/Admin/RfaController.php
@@ -1442,6 +1442,30 @@ class RfaController extends Controller
 
         }
 
+        if($rfa->construction_contract->code == "C4-2"){
+            $issue_by = '( นายธีรพงษ์ วิธีเจริญ )';
+            $issuer_jobtitle = 'ผู้รับมอบอำนาจ';
+            $issue_position_lf = 255;
+            $issue_position_lf_sub = 489;
+            $constructor_name = 'Unique Engineering and Construction Public Company Limited';
+            $constructor_code = 'UNIQUE';
+            $logo_path = public_path('png-asset/UNIQUE_logo.png');
+            $logo_h = 40;
+            $logo_w = 40;
+            $logo_top = 109;
+            $logo_left = 690;
+            $stamp_path =  '';
+            $signature_path =  '';
+            $signature_size_h = 50;
+            $signature_size_w = 50;
+            $signature_position_top = 415;
+            $signature_position_left = 278;
+            $stamp_size_h = 120;
+            $stamp_size_w = 120;
+            $contract_name = 'Contract ' . $rfa->construction_contract->code . ' : ' . $rfa->construction_contract->name;
+
+        }
+
         if($rfa->construction_contract->code == "C4-6"){
             $issue_by = '( นายธีรพงษ์ วิธีเจริญ )';
             $issuer_jobtitle = 'ผู้รับมอบอำนาจ';
