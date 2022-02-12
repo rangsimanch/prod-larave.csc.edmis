9408dc78908c62986dae154e6cc737d99ab47048
rangsimanch <12jniw.t@gmail.com>
2022-02-12 19:56:15 +0700

    Update index.blade.php

--- resources/views/admin/rfas/index.blade.php
+++ resources/views/admin/rfas/index.blade.php
@@ -231,6 +231,9 @@
     serverSide: true,
     retrieve: true,
     aaSorting: [],
+    columnDefs: [
+        {"className": "dt-center", "targets": "_all"}
+      ],
     ajax: "{{ route('admin.rfas.index') }}",
     columns: [
       { data: 'placeholder', name: 'placeholder' },
