44974924c1b44febecd22178065b2f2dc5ac04a6
rangsimanch <12jniw.t@gmail.com>
2022-03-12 10:24:20 +0700

    Update SwnController.php

--- app/Http/Controllers/Admin/SwnController.php
+++ app/Http/Controllers/Admin/SwnController.php
@@ -569,7 +569,7 @@ class SwnController extends Controller
        
         $mpdf->SetHTMLHeader($html,'0',true);
         $html = "<div style=\" padding-left: 80px; padding-right:40px; padding-bottom:-15px; \">";
-        $html .= "<div style=\"font-size: 10px; padding-right:80px; position:absolute;top:380px;left:120px;\">" . $description  . "</div>";
+        $html .= "<div style=\"font-size: 10px; padding-right:75px; position:absolute;top:380px;left:115px;\">" . $description  . "</div>";
         $html .= "</div>";
        
         $mpdf->WriteHTML($html);
