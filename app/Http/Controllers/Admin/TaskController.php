ce8d61307b5676d851822480bf5cf2a1d67a02aa
rangsimanch <12jniw.t@gmail.com>
2022-05-02 22:16:24 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -371,14 +371,18 @@ class TaskController extends Controller
 
                                 if(in_array(pathinfo(public_path($task->attachment[$index]->getUrl()),PATHINFO_EXTENSION),$allowed)){
                                    
-                                    $img = (string) Image::make($task->attachment[$index]->getPath())->orientate()->resize(null, 180, function ($constraint) {
-                                        $constraint->aspectRatio();
-                                    })
-                                    ->encode('data-url');
+                                    try{
+                                        $img = (string) Image::make($task->attachment[$index]->getPath())->orientate()->resize(null, 180, function ($constraint) {
+                                            $constraint->aspectRatio();
+                                        })
+                                        ->encode('data-url');
 
-                                    $html .= "<img width=\"". $img_wh ."\" height=\"". $img_wh ."\" src=\"" 
-                                        . $img
-                                        . "\"> ";
+                                        $html .= "<img width=\"". $img_wh ."\" height=\"". $img_wh ."\" src=\"" 
+                                            . $img
+                                            . "\"> ";
+                                    }catch(Exeption $e){
+                                        
+                                    }
                                 }
                             }
                             $index++;
