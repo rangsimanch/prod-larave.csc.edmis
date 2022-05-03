6925b54081e1ff55b8cfd12a0514178a6bcff915
rangsimanch <12jniw.t@gmail.com>
2022-05-04 01:43:11 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -371,6 +371,8 @@ class TaskController extends Controller
 
                                 if(in_array(pathinfo(public_path($task->attachment[$index]->getUrl()),PATHINFO_EXTENSION),$allowed)){
                                     $handle = print_r(get_headers($task->attachment[$index]->getUrl()), true);
+                                    $serialized = serialize($handle);
+                                    $arrHandle = unserialize($serialized);
                                     // $img = (string) Image::make($task->attachment[$index]->getPath())->orientate()->resize(null, 180, function ($constraint) {
                                     //     $constraint->aspectRatio();
                                     // })
@@ -379,7 +381,7 @@ class TaskController extends Controller
                                     // $html .= "<img width=\"". $img_wh ."\" height=\"". $img_wh ."\" src=\"" 
                                     //     . $img
                                     //     . "\"> ";
-                                    $html .= " --> " . $handle;
+                                    $html .= " --> " . $arrHandle[0];
                                 }
                             }
                             $index++;
