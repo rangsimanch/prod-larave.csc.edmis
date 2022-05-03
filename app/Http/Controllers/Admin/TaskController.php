2c66c9752e5cca68030ab8133e501d104cc79664
rangsimanch <12jniw.t@gmail.com>
2022-05-04 01:39:03 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -370,7 +370,7 @@ class TaskController extends Controller
                                 }
 
                                 if(in_array(pathinfo(public_path($task->attachment[$index]->getUrl()),PATHINFO_EXTENSION),$allowed)){
-                                    $handle = print_r(get_headers($task->attachment[$index]->getUrl()), true);
+                                    $handle = explode(get_headers($task->attachment[$index]->getUrl()), true);
                                     // $img = (string) Image::make($task->attachment[$index]->getPath())->orientate()->resize(null, 180, function ($constraint) {
                                     //     $constraint->aspectRatio();
                                     // })
@@ -379,7 +379,7 @@ class TaskController extends Controller
                                     // $html .= "<img width=\"". $img_wh ."\" height=\"". $img_wh ."\" src=\"" 
                                     //     . $img
                                     //     . "\"> ";
-                                    $html .= " --> " . $handle[1];
+                                    $html .= " --> " . $handle[0];
                                 }
                             }
                             $index++;
