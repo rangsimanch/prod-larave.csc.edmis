3001894846d58343f7f434260ba181e591cc3f00
rangsimanch <12jniw.t@gmail.com>
2022-05-04 01:34:09 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -379,7 +379,7 @@ class TaskController extends Controller
                                     // $html .= "<img width=\"". $img_wh ."\" height=\"". $img_wh ."\" src=\"" 
                                     //     . $img
                                     //     . "\"> ";
-                                    $html .= " --> " . $handle[0];
+                                    $html .= " --> " . $handle;
                                 }
                             }
                             $index++;
