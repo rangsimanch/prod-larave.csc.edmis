2c76a628b9fa10408ab57874d9a46a81a6407833
rangsimanch <12jniw.t@gmail.com>
2023-08-11 00:21:59 +0700

    Circulation_General_All_Contract_Update_082023

--- app/Http/Controllers/Admin/RfaController.php
+++ app/Http/Controllers/Admin/RfaController.php
@@ -1491,7 +1491,7 @@ class RfaController extends Controller
         //Circulation of Work
 
         $mpdf->AddPage();
-        $pagecount = $mpdf->SetSourceFile(public_path('pdf-asset/Circulation_General_All_Contract_Update.pdf'));
+        $pagecount = $mpdf->SetSourceFile(public_path('pdf-asset/Circulation_General_All_Contract_Update_082023.pdf'));
         $tplId = $mpdf->ImportPage($pagecount);
         $mpdf->UseTemplate($tplId);   
 
