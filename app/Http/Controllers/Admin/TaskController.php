6dd16c6d37013fa8b9d6fd79f01d1fc85d860412
rangsimanch <12jniw.t@gmail.com>
2024-02-05 14:44:08 +0700

    Update TaskController.php

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -372,9 +372,10 @@ class TaskController extends Controller
 
                 // $mpdf->AddPage('P','','','','','','',60,55);
                 $mpdf->SetDocTemplate(public_path('pdf-asset/activity.pdf'),true);
+                $mpdf->AddPage($page_size['orientation'],'','','','','','',60,55);
+
                 // $mpdf->UseTemplate($import_page, 0, 0, $page_size['width'], $page_size['height'], true);
 
-                $mpdf->AddPage($page_size['orientation'],'','','','','','',60,55);
 
 
 
@@ -542,9 +543,8 @@ class TaskController extends Controller
                             for($page = 1; $page <= $pagecount; $page++){
                                 $tplId = $mpdf->importPage($page);
                                 $size = $mpdf->getTemplateSize($tplId);
-                                $mpdf->AddPage($size['orientation']);
                                 $mpdf->UseTemplate($tplId, 0, 0, $size['width'], $size['height'], true);
-                                $mpdf->RestartDocTemplate(); 
+                                $mpdf->AddPage($size['orientation']);
                             }         
                         }
                     }catch(exeption $e){
