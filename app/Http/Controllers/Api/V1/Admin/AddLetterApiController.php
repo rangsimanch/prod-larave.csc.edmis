fa693b0227b10437210751d06940c0fafd398045
rangsimanch <12jniw.t@gmail.com>
2024-06-10 22:17:11 +0700

    API 5001 ID

--- app/Http/Controllers/Api/V1/Admin/AddLetterApiController.php
+++ app/Http/Controllers/Api/V1/Admin/AddLetterApiController.php
@@ -21,7 +21,7 @@ class AddLetterApiController extends Controller
         abort_if(Gate::denies('add_letter_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         // $addLetters = AddLetter::with([ 'sender', 'receiver', 'construction_contract', 'team'])->orderBy('id', 'desc')->limit(500)->get();
         $addLetters = AddLetter::with([ 'sender', 'receiver', 'construction_contract', 'team'])
-        ->where('id', '>=', 1)
+        ->where('id', '>=', 5001)
         ->orderBy('id', 'asc')->limit(5000)->get();
 
         
