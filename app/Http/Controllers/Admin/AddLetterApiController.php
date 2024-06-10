7a33181f25be14c1621900199b348fca8d355ac1
rangsimanch <12jniw.t@gmail.com>
2024-06-10 22:22:42 +0700

    Letter API Complete

--- app/Http/Controllers/Admin/AddLetterApiController.php
+++ app/Http/Controllers/Admin/AddLetterApiController.php
@@ -19,11 +19,11 @@ class AddLetterApiController extends Controller
     public function index()
     {
         abort_if(Gate::denies('add_letter_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-        // $addLetters = AddLetter::with([ 'sender', 'receiver', 'construction_contract', 'team'])->orderBy('id', 'desc')->limit(500)->get();
+        $addLetters = AddLetter::with([ 'sender', 'receiver', 'construction_contract', 'team'])->orderBy('id', 'desc')->limit(500)->get();
         
-        $addLetters = AddLetter::with([ 'sender', 'receiver', 'construction_contract', 'team'])
-        ->where('id', '>=', 10209)
-        ->orderBy('id', 'asc')->limit(5000)->get();
+        // $addLetters = AddLetter::with([ 'sender', 'receiver', 'construction_contract', 'team'])
+        // ->where('id', '>=', 10209)
+        // ->orderBy('id', 'asc')->limit(5000)->get();
 
 
         return AddLetterResource::collection($addLetters)->response()->setData(
