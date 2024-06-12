d488211a66787d65ea7db0b5bf138c0225af933e
rangsimanch <12jniw.t@gmail.com>
2024-06-12 17:35:12 +0700

    Update RfaApiController.php

--- app/Http/Controllers/Api/V1/Admin/RfaApiController.php
+++ app/Http/Controllers/Api/V1/Admin/RfaApiController.php
@@ -11,6 +11,7 @@ use App\Rfa;
 use Gate;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Carbon\Carbon;
 
 class RfaApiController extends Controller
 {
@@ -42,13 +43,11 @@ class RfaApiController extends Controller
 
                 $updatedDate = '';
                 try {
-                    $date = new DateTime($rfa->updated_at);
+                    $date = Carbon::parse($rfa->updated_at);
                     $updatedDate = $date->format('d/m/Y');
                 }catch(exeption $e){
                     $updatedDate = '';
                 }
-    
-               
 
                 return [
                     'id' => $rfa->id,
@@ -82,27 +81,27 @@ class RfaApiController extends Controller
 
     public function store(StoreRfaRequest $request)
     {
-        $rfa = Rfa::create($request->all());
+        // $rfa = Rfa::create($request->all());
 
-        if ($request->input('file_upload_1', false)) {
-            $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('file_upload_1')))->toMediaCollection('file_upload_1');
-        }
+        // if ($request->input('file_upload_1', false)) {
+        //     $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('file_upload_1')))->toMediaCollection('file_upload_1');
+        // }
 
-        if ($request->input('commercial_file_upload', false)) {
-            $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('commercial_file_upload')))->toMediaCollection('commercial_file_upload');
-        }
+        // if ($request->input('commercial_file_upload', false)) {
+        //     $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('commercial_file_upload')))->toMediaCollection('commercial_file_upload');
+        // }
 
-        if ($request->input('document_file_upload', false)) {
-            $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('document_file_upload')))->toMediaCollection('document_file_upload');
-        }
+        // if ($request->input('document_file_upload', false)) {
+        //     $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('document_file_upload')))->toMediaCollection('document_file_upload');
+        // }
 
-        if ($request->input('submittals_file', false)) {
-            $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('submittals_file')))->toMediaCollection('submittals_file');
-        }
+        // if ($request->input('submittals_file', false)) {
+        //     $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('submittals_file')))->toMediaCollection('submittals_file');
+        // }
 
-        return (new RfaResource($rfa))
-            ->response()
-            ->setStatusCode(Response::HTTP_CREATED);
+        // return (new RfaResource($rfa))
+        //     ->response()
+        //     ->setStatusCode(Response::HTTP_CREATED);
     }
 
     public function show(Rfa $rfa)
@@ -148,50 +147,50 @@ class RfaApiController extends Controller
 
     public function update(UpdateRfaRequest $request, Rfa $rfa)
     {
-        $rfa->update($request->all());
+        // $rfa->update($request->all());
 
-        if ($request->input('file_upload_1', false)) {
-            if (!$rfa->file_upload_1 || $request->input('file_upload_1') !== $rfa->file_upload_1->file_name) {
-                $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('file_upload_1')))->toMediaCollection('file_upload_1');
-            }
-        } elseif ($rfa->file_upload_1) {
-            $rfa->file_upload_1->delete();
-        }
+        // if ($request->input('file_upload_1', false)) {
+        //     if (!$rfa->file_upload_1 || $request->input('file_upload_1') !== $rfa->file_upload_1->file_name) {
+        //         $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('file_upload_1')))->toMediaCollection('file_upload_1');
+        //     }
+        // } elseif ($rfa->file_upload_1) {
+        //     $rfa->file_upload_1->delete();
+        // }
 
-        if ($request->input('commercial_file_upload', false)) {
-            if (!$rfa->commercial_file_upload || $request->input('commercial_file_upload') !== $rfa->commercial_file_upload->file_name) {
-                $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('commercial_file_upload')))->toMediaCollection('commercial_file_upload');
-            }
-        } elseif ($rfa->commercial_file_upload) {
-            $rfa->commercial_file_upload->delete();
-        }
+        // if ($request->input('commercial_file_upload', false)) {
+        //     if (!$rfa->commercial_file_upload || $request->input('commercial_file_upload') !== $rfa->commercial_file_upload->file_name) {
+        //         $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('commercial_file_upload')))->toMediaCollection('commercial_file_upload');
+        //     }
+        // } elseif ($rfa->commercial_file_upload) {
+        //     $rfa->commercial_file_upload->delete();
+        // }
 
-        if ($request->input('document_file_upload', false)) {
-            if (!$rfa->document_file_upload || $request->input('document_file_upload') !== $rfa->document_file_upload->file_name) {
-                $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('document_file_upload')))->toMediaCollection('document_file_upload');
-            }
-        } elseif ($rfa->document_file_upload) {
-            $rfa->document_file_upload->delete();
-        }
+        // if ($request->input('document_file_upload', false)) {
+        //     if (!$rfa->document_file_upload || $request->input('document_file_upload') !== $rfa->document_file_upload->file_name) {
+        //         $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('document_file_upload')))->toMediaCollection('document_file_upload');
+        //     }
+        // } elseif ($rfa->document_file_upload) {
+        //     $rfa->document_file_upload->delete();
+        // }
 
-        if ($request->input('submittals_file', false)) {
-            if (!$rfa->submittals_file || $request->input('submittals_file') !== $rfa->submittals_file->file_name) {
-                $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('submittals_file')))->toMediaCollection('submittals_file');
-            }
-        } elseif ($rfa->submittals_file) {
-            $rfa->submittals_file->delete();
-        }
+        // if ($request->input('submittals_file', false)) {
+        //     if (!$rfa->submittals_file || $request->input('submittals_file') !== $rfa->submittals_file->file_name) {
+        //         $rfa->addMedia(storage_path('tmp/uploads/' . $request->input('submittals_file')))->toMediaCollection('submittals_file');
+        //     }
+        // } elseif ($rfa->submittals_file) {
+        //     $rfa->submittals_file->delete();
+        // }
 
-        return (new RfaResource($rfa))
-            ->response()
-            ->setStatusCode(Response::HTTP_ACCEPTED);
+        // return (new RfaResource($rfa))
+        //     ->response()
+        //     ->setStatusCode(Response::HTTP_ACCEPTED);
     }
 
     public function destroy(Rfa $rfa)
     {
         abort_if(Gate::denies('rfa_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $rfa->delete();
+        // $rfa->delete();
 
         return response(null, Response::HTTP_NO_CONTENT);
     }
