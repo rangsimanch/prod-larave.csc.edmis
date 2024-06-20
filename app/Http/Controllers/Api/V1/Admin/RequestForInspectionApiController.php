6c545e34ffff883bbfaf25ec244da0907d4ebdf0
rangsimanch <12jniw.t@gmail.com>
2024-06-20 23:33:21 +0700

    Update RequestForInspectionApiController.php

--- app/Http/Controllers/Api/V1/Admin/RequestForInspectionApiController.php
+++ app/Http/Controllers/Api/V1/Admin/RequestForInspectionApiController.php
@@ -11,6 +11,8 @@ use App\RequestForInspection;
 use Gate;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Carbon\Carbon;
+
 
 class RequestForInspectionApiController extends Controller
 {
@@ -20,24 +22,62 @@ class RequestForInspectionApiController extends Controller
     {
         abort_if(Gate::denies('request_for_inspection_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return new RequestForInspectionResource(RequestForInspection::with(['construction_contract', 'wbs_level_1', 'bill', 'wbs_level_3', 'item_1', 'item_2', 'item_3', 'requested_by', 'contact_person', 'team'])->get());
+        // $rfns = RequestForInspection::with(['construction_contract', 'wbs_level_1', 'bill', 'wbs_level_3', 'item_1', 'item_2', 'item_3', 'requested_by', 'contact_person', 'team'])
+        // ->orderBy('updated_at', 'desc')->limit(500)->get();
+
+
+        $rfns = RequestForInspection::with(['construction_contract', 'wbs_level_1', 'bill', 'wbs_level_3', 'item_1', 'item_2', 'item_3', 'requested_by', 'contact_person', 'team'])
+        ->where('id', '>=', 1)
+        ->orderBy('id', 'asc')->limit(5000)->get();
+
+
+        return RequestForInspection::collection($rfns)->response()->setData(
+            $rfns->map(function ($rfn) {
+                $file_upload_link = [];
+                foreach ($rfn->files_upload as $media) {
+                    $file_upload_link[] = $media->getUrl();
+                }
+
+        
+                $updatedDate = '';
+                try {
+                    $date = Carbon::parse($rfn->updated_at);
+                    $updatedDate = $date->format('d/m/Y');
+                }catch(exeption $e){
+                    $updatedDate = '';
+                }
+
+                return [
+                    'id' => $rfn->id,
+                    'construction_contract' => $rfn->construction_contract ? $rfn->construction_contract->code : '',
+                    'boq' => $rfn->bill ? $rfn->bill->name : '',
+                    'title' => $rfn->subject ? $rfn->subject : '',
+                    'document_number' => $rfn->ref_no ? $rfn->ref_no : '',
+                    'ipa' => $rfn->ipa ? $rfn->ipa : '',
+                    'submit_date' => $rfn->submit_date,
+                    'file_upload_link' => implode(', ', $file_upload_link),
+                    'updated_at' => $updatedDate,
+                    // add any other fields you want to include in the response
+                ];
+            })
+        );
     }
 
     public function store(StoreRequestForInspectionRequest $request)
     {
-        $requestForInspection = RequestForInspection::create($request->all());
+        // $requestForInspection = RequestForInspection::create($request->all());
 
-        if ($request->input('files_upload', false)) {
-            $requestForInspection->addMedia(storage_path('tmp/uploads/' . $request->input('files_upload')))->toMediaCollection('files_upload');
-        }
+        // if ($request->input('files_upload', false)) {
+        //     $requestForInspection->addMedia(storage_path('tmp/uploads/' . $request->input('files_upload')))->toMediaCollection('files_upload');
+        // }
 
-        if ($request->input('loop_file_upload', false)) {
-            $requestForInspection->addMedia(storage_path('tmp/uploads/' . $request->input('loop_file_upload')))->toMediaCollection('loop_file_upload');
-        }
+        // if ($request->input('loop_file_upload', false)) {
+        //     $requestForInspection->addMedia(storage_path('tmp/uploads/' . $request->input('loop_file_upload')))->toMediaCollection('loop_file_upload');
+        // }
 
-        return (new RequestForInspectionResource($requestForInspection))
-            ->response()
-            ->setStatusCode(Response::HTTP_CREATED);
+        // return (new RequestForInspectionResource($requestForInspection))
+        //     ->response()
+        //     ->setStatusCode(Response::HTTP_CREATED);
     }
 
     public function show(RequestForInspection $requestForInspection)
@@ -49,43 +89,43 @@ class RequestForInspectionApiController extends Controller
 
     public function update(UpdateRequestForInspectionRequest $request, RequestForInspection $requestForInspection)
     {
-        $requestForInspection->update($request->all());
+        // $requestForInspection->update($request->all());
 
-        if ($request->input('files_upload', false)) {
-            if (!$requestForInspection->files_upload || $request->input('files_upload') !== $requestForInspection->files_upload->file_name) {
-                if ($requestForInspection->files_upload) {
-                    $requestForInspection->files_upload->delete();
-                }
+        // if ($request->input('files_upload', false)) {
+        //     if (!$requestForInspection->files_upload || $request->input('files_upload') !== $requestForInspection->files_upload->file_name) {
+        //         if ($requestForInspection->files_upload) {
+        //             $requestForInspection->files_upload->delete();
+        //         }
 
-                $requestForInspection->addMedia(storage_path('tmp/uploads/' . $request->input('files_upload')))->toMediaCollection('files_upload');
-            }
-        } elseif ($requestForInspection->files_upload) {
-            $requestForInspection->files_upload->delete();
-        }
+        //         $requestForInspection->addMedia(storage_path('tmp/uploads/' . $request->input('files_upload')))->toMediaCollection('files_upload');
+        //     }
+        // } elseif ($requestForInspection->files_upload) {
+        //     $requestForInspection->files_upload->delete();
+        // }
 
-        if ($request->input('loop_file_upload', false)) {
-            if (!$requestForInspection->loop_file_upload || $request->input('loop_file_upload') !== $requestForInspection->loop_file_upload->file_name) {
-                if ($requestForInspection->loop_file_upload) {
-                    $requestForInspection->loop_file_upload->delete();
-                }
+        // if ($request->input('loop_file_upload', false)) {
+        //     if (!$requestForInspection->loop_file_upload || $request->input('loop_file_upload') !== $requestForInspection->loop_file_upload->file_name) {
+        //         if ($requestForInspection->loop_file_upload) {
+        //             $requestForInspection->loop_file_upload->delete();
+        //         }
 
-                $requestForInspection->addMedia(storage_path('tmp/uploads/' . $request->input('loop_file_upload')))->toMediaCollection('loop_file_upload');
-            }
-        } elseif ($requestForInspection->loop_file_upload) {
-            $requestForInspection->loop_file_upload->delete();
-        }
+        //         $requestForInspection->addMedia(storage_path('tmp/uploads/' . $request->input('loop_file_upload')))->toMediaCollection('loop_file_upload');
+        //     }
+        // } elseif ($requestForInspection->loop_file_upload) {
+        //     $requestForInspection->loop_file_upload->delete();
+        // }
 
-        return (new RequestForInspectionResource($requestForInspection))
-            ->response()
-            ->setStatusCode(Response::HTTP_ACCEPTED);
+        // return (new RequestForInspectionResource($requestForInspection))
+        //     ->response()
+        //     ->setStatusCode(Response::HTTP_ACCEPTED);
     }
 
     public function destroy(RequestForInspection $requestForInspection)
     {
         abort_if(Gate::denies('request_for_inspection_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $requestForInspection->delete();
+        // $requestForInspection->delete();
 
-        return response(null, Response::HTTP_NO_CONTENT);
+        // return response(null, Response::HTTP_NO_CONTENT);
     }
 }
