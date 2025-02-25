c6d8036db079dcfb582c65830fde9b7722eddf8e
rangsimanch <12jniw.t@gmail.com>
2025-02-25 20:58:10 +0700

    Update SwnController.php

--- app/Http/Controllers/Admin/SwnController.php
+++ app/Http/Controllers/Admin/SwnController.php
@@ -548,7 +548,7 @@ class SwnController extends Controller
 
         $conditional_accepted = $swn->conditional_accepted ?? '';
 
-        $html = "<div style=\"font-size: 10px; text-align: center; font-weight: bold; color:#1F4E78; padding-top:82px;\">" . $contract_name  . "</div>";
+        $html = "<div style=\"font-size: 10px; text-align: center; font-weight: bold; color:#1F4E78; padding-top:10px;\">" . $contract_name  . "</div>";
         $html .= "<div style=\"font-size: 12px; position:absolute;top:195px;left:120px;\">" . $send_to  . "</div>";
         $html .= "<div style=\"font-size: 12px; position:absolute;top:195px;left:370px;\">" . $submit_date  . "</div>";
         $html .= "<div style=\"font-size: 10px; position:absolute;top:195px;left:510px;\">" . $document_number  . "</div>";
