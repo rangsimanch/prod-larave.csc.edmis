750688f7d4b97419008674ea57de331e87dc555c
rangsimanch <12jniw.t@gmail.com>
2025-02-25 19:28:35 +0700

    Update SwnController.php

--- app/Http/Controllers/Admin/SwnController.php
+++ app/Http/Controllers/Admin/SwnController.php
@@ -742,13 +742,13 @@ class SwnController extends Controller
         $html .= "<div style=\"font-size: 8px; position:absolute;top:990px;left:360px;\">" . $auditing_date  . "</div>";
         $html .= "<div style=\"font-size: 8px; position:absolute;top:990px;left:600px;\">" . $auditing_date  . "</div>";
 
-        $mpdf->SetHTMLHeader($html,'0',false);
-        $html = "<div style=\" padding-left: 80px; padding-right:40px; padding-bottom:-15px; \">";
-        $html .= "<div style=\"font-size: 9px; padding-right:50px; position:absolute;top:380px;left:110px;LINE-HEIGHT:15px;\">" . $description  . "</div>";
-        $html .= "</div>";
+        $mpdf->SetHTMLHeader($html,'0',true);
+        $des = "<div style=\" padding-left: 80px; padding-right:40px; padding-bottom:-15px; \">";
+        $des .= "<div style=\"font-size: 9px; padding-right:50px; position:absolute;top:380px;left:110px;LINE-HEIGHT:15px;\">" . $description  . "</div>";
+        $des .= "</div>";
 
-        $mpdf->WriteHTML($html);
-        $html = "";
+        $mpdf->WriteHTML($des);
+        // $html = "";
         $mpdf->SetHTMLHeader($html,'0',true);
 
 
