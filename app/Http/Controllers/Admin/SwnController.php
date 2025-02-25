d14459772e5f11cd880438786bbd2f7a3594629e
rangsimanch <12jniw.t@gmail.com>
2025-02-25 20:59:46 +0700

    Update SwnController.php

--- app/Http/Controllers/Admin/SwnController.php
+++ app/Http/Controllers/Admin/SwnController.php
@@ -517,7 +517,7 @@ class SwnController extends Controller
         // $tplId = $mpdf->ImportPage($pagecount);
         // $mpdf->UseTemplate($tplId);
         $mpdf->SetDocTemplate(public_path('pdf-asset/SWN_Template_Section_1.pdf'),true);
-        $mpdf->AddPage('P','','','','','','',90,140);
+        $mpdf->AddPage('P','','','','','','',110,140);
 
 
         // Setting Data
