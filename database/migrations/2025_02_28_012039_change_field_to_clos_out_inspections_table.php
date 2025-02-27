023a1d8fc6f52362db023de61ee7644533a344f4
rangsimanch <12jniw.t@gmail.com>
2025-02-28 01:21:37 +0700

    Create 2025_02_28_012039_change_field_to_clos_out_inspections_table.php

---
+++ database/migrations/2025_02_28_012039_change_field_to_clos_out_inspections_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeFieldToClosOutInspectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('clos_out_inspections', function (Blueprint $table) {
+            //
+            $table->longText('subject')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('clos_out_inspections', function (Blueprint $table) {
+            //
+        });
+    }
+}
