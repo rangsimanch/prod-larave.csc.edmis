8f083af7b23a26acdb0d07fe3c0105c600a633af
rangsimanch <12jniw.t@gmail.com>
2026-01-26 13:56:13 +0700

    Remove logout button from construction contract selection page

--- resources/views/auth/selectConstructionContract.blade.php
+++ resources/views/auth/selectConstructionContract.blade.php
@@ -21,16 +21,6 @@
                            </div>
                         </div>
                     {!! Form::close() !!}
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <button type="submit" class="btn btn-default">
-                                    {{ trans('global.logout') ?? __('Logout') }}
-                                </button>
-                            </form>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
