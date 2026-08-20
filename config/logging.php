295f092af0e9d94ab6c90d1a7ac8d9a9d1416fcf
rangsimanch <12jniw.t@gmail.com>
2026-08-21 02:03:22 +0700

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver'            => 'stack',
-            'channels'          => ['single', 'bugsnag'],
+            'channels'          => ['daily', 'bugsnag'],
             'ignore_exceptions' => false,
         ],
 
